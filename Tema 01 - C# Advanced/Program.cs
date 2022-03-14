9fcd8d96205dd3d202ae33c49a638f81badfac2e
VictorD-98 <79086823+VictorD-98@users.noreply.github.com>
2022-03-14 22:15:33 +0200

    Add files via upload

---
+++ Tema 01 - C# Advanced/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+        public class Set<T>
+
+        {
+            public string SetData { get; set; }
+            public int DataID { get; set; }
+            public List<string> ListData = new List<string>();
+            public List<string> ListID = new List<string>();
+
+            public string PrintSet()
+            { return "ID" + DataID + " Data:" + SetData; }
+            bool Contains(T item)
+            {
+
+                if(ListData.Any(x=>x.data.Equals(item.data)))
+
+                //
+               int dx = ListData.IndexOf(item);
+
+                if (dx > 0) return true;
+                return false;
+
+
+
+            }
+
+            void Insert(T item)
+            {
+                if (Contains(item)) 
+                try
+                {
+                        ListData.Insert(ListData.Count, item.data);
+                        ListID.Insert(ListID.Count, item.ID);
+
+                    }
+                catch
+                { }
+                finally
+                { }
+            }
+            void Remove(T item)
+            {   var rmv=ListData.Single(r => r.)
+            
+            
+            }
+            
+            Set<T> Merge(Set<T> other)
+            { }
+            void Filter()
+            { }
+
+
+        }
+    }
+}
